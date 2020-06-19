dfd0ceee10ab36588d4d7fd8901774be32bb8e66
Thayol <thayol@zovguran.net>
2020-06-19 21:34:03 +0200

    Android Studio autogeneration

---
+++ android-champmastery/app/src/main/java/net/zovguran/lolchampionmastery/PlayerMasteryView.kt
@@ -0,0 +1,11 @@
+package net.zovguran.lolchampionmastery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerMasteryView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_mastery_view)
+    }
+}
